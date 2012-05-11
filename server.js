062e72afe5e188816c7d20e78e8e86e8c91161ea
Fil Maj <maj.fil@gmail.com>
2012-05-11 14:54:00 -0700

    posts server results now biatccchhh

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = module.exports = express.createServer();
+
+var io = require('socket.io').listen(app);
 
 // Express Configuration
 
@@ -19,6 +22,9 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+var host = 'http://localhost';
+var port = 3000;
+
 // ----
 // ROUTES
 // ----
@@ -26,17 +32,42 @@ app.configure('production', function(){
 // Simple queue of results coming in 
 var results = [];
 
+// Max number of results to keep in memory / show
+var max = 5;
+
+// Result template
+var tmpl = '<div class="result">{result}</div>';
+
+// Home page template HTML
+var html = fs.readFileSync('static/index.html', 'utf-8')
+                .split("url = ''").join("url = '" + host + ":" + port + "'")
+                .split('max = 0').join('max = ' + max)
+                .split("tmpl = ''").join("tmpl = '" + tmpl + "'");
+
+
+function output(r) {
+  if (r.length === 0) {
+    return "<h3>No results</h3>";
+  } else {
+    var resHtml = r.reverse().map(function(t) { return tmpl.split('{result}').join(JSON.stringify(t)); });
+    return resHtml.join('');
+  }
+}
+
 // POST a test result to server
 app.post('/result', function(req, res) {
-  if (results.length >= 20) results.shift();
-  results.push(req.body);
+  if (results.length >= max) results.shift();
+  var r = req.body;
+  results.push(r);
   res.send({success:true});
+  io.sockets.emit('result', r);
 });
 
 // GET the homepage
 app.get('/', function(req, res) {
+  res.send(html.split('<!-- RESULTS -->').join(output(results)));
 });
 
-app.listen(3000);
+app.listen(port);
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
